c6863f7929037d99c1dc826bf7dc4958e391b1ce
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2024-12-07 15:52:40 +0100

    refactor: link zod schema to status type variable

--- src/server/db/types.ts
+++ src/server/db/types.ts
@@ -2,5 +2,5 @@ import type { applications } from "./schema"
 
 export type Application = typeof applications.$inferSelect
 
-export const statusValues = ["pending", "accepted", "declined", "interview"] as const
+export const statusValues = ["draft", "applied", "accepted", "declined", "interview"] as const
 export type StatusType = (typeof statusValues)[number]
--- src/server/db/zod.ts
+++ src/server/db/zod.ts
@@ -1,13 +1,24 @@
 import { createInsertSchema } from "drizzle-zod"
 import { applications } from "./schema"
 import { z } from "zod"
+import { statusValues } from "./types"
 
 export const insertApplicationSchema = createInsertSchema(applications, {
-	status: z.enum(["pending", "accepted", "declined", "interview"], {
+	status: z.enum(statusValues, {
 		required_error: "Status is required",
 	}),
 	vacancyUrl: z.string().url(),
 }).omit({
 	id: true,
+	createdAt: true,
+	updatedAt: true,
+})
+export const updateApplicationSchema = createInsertSchema(applications, {
+	id: z.number(),
+	status: z.enum(["pending", "accepted", "declined", "interview"], {
+		required_error: "Status is required",
+	}),
+	vacancyUrl: z.string().url(),
+}).omit({
 	createdAt: true,
 })
