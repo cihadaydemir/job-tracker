9461b85a981c479b228d58711baf3a91c0af2dc4
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2024-12-07 18:08:30 +0100

    fix: suppress hydration warning for theme-provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<html lang="en" className={`${GeistSans.variable}`}>
+		<html lang="en" className={`${GeistSans.variable}`} suppressHydrationWarning>
 			<body>
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
 					<TRPCReactProvider>
