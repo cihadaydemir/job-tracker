f5e29f12dcb37f24938a1f42599905aa708ef1d2
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2024-12-07 22:11:42 +0100

    chore: replace favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,12 @@ import { ClerkProvider } from "@clerk/nextjs"
 export const metadata: Metadata = {
 	title: "Application Tracker",
 	description: "build by chd",
-	icons: [{ rel: "icon", url: "/favicon.ico" }],
+	icons: [
+		{
+			rel: "icon",
+			url: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💼</text></svg>",
+		},
+	],
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
