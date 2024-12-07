7ef5f40ad519573da0742a6f02cc4f7ff8a066b4
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2024-12-07 15:21:35 +0100

    refactor: outsource form to own component

--- src/app/_components/create-application-dialog.tsx
+++ src/app/_components/create-application-dialog.tsx
@@ -1,52 +1,20 @@
 "use client"
 
-import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "~/components/ui/button"
 import {
 	Dialog,
 	DialogContent,
 	DialogDescription,
-	DialogFooter,
 	DialogHeader,
 	DialogTitle,
 	DialogTrigger,
 } from "~/components/ui/dialog"
 
-import { revalidatePath } from "next/cache"
-import { useForm } from "react-hook-form"
-import type { z } from "zod"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form"
-import { Input } from "~/components/ui/input"
-import { insertApplicationSchema } from "~/server/db/zod"
-import { api } from "~/trpc/react"
-import { toast } from "sonner"
 import { useState } from "react"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "~/components/ui/select"
-import { statusValues } from "~/server/db/types"
+import { CreateApplicationForm } from "./create-application-form"
 
 export const CreateApplicationDialog = () => {
 	const [isDialogOpen, setIsDialogOpen] = useState(false)
-
-	const createApplicationMutation = api.applicaiton.create.useMutation({
-		onSuccess: (data, variables, context) => {
-			console.log("data success", data)
-			setIsDialogOpen(false)
-			toast.success("Application created successfully ✅")
-			revalidatePath("/", "layout")
-		},
-	})
-
-	const form = useForm<z.infer<typeof insertApplicationSchema>>({
-		resolver: zodResolver(insertApplicationSchema),
-		defaultValues: {
-			companyName: "",
-			status: "pending",
-		},
-	})
-
-	function onSubmit(values: z.infer<typeof insertApplicationSchema>) {
-		createApplicationMutation.mutate(values)
-	}
 
 	return (
 		<Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
@@ -59,50 +27,7 @@ export const CreateApplicationDialog = () => {
 					<DialogDescription>Add your job application to the list.</DialogDescription>
 				</DialogHeader>
 				<div>
-					<Form {...form}>
-						<form onSubmit={form.handleSubmit(onSubmit)}>
-							<FormField
-								control={form.control}
-								name="companyName"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Company Name</FormLabel>
-										<FormControl>
-											<Input placeholder="Company Name" {...field} />
-										</FormControl>
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<FormField
-								control={form.control}
-								name="status"
-								render={({ field }) => (
-									<FormItem>
-										<FormLabel>Status</FormLabel>
-										<FormControl>
-											<Select {...field}>
-												<SelectTrigger className="w-[180px]">
-													<SelectValue placeholder="Select the application status" />
-												</SelectTrigger>
-												<SelectContent>
-													{statusValues.map((value) => (
-														<SelectItem key={value} value={value}>
-															{value}
-														</SelectItem>
-													))}
-												</SelectContent>
-											</Select>
-										</FormControl>
-										<FormMessage />
-									</FormItem>
-								)}
-							/>
-							<Button type="submit" disabled={!form.formState.isValid}>
-								Add
-							</Button>
-						</form>
-					</Form>
+					<CreateApplicationForm setIsDialogOpen={setIsDialogOpen} />
 				</div>
 			</DialogContent>
 		</Dialog>
---
+++ src/app/_components/create-application-form.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import React from "react"
+import { useForm } from "react-hook-form"
+import type { z } from "zod"
+import { insertApplicationSchema } from "~/server/db/zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "~/components/ui/form"
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@radix-ui/react-select"
+
+import { Button } from "~/components/ui/button"
+import { statusValues } from "~/server/db/types"
+import { Input } from "~/components/ui/input"
+
+import { api } from "~/trpc/react"
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
+
+interface CreateApplicationFormProps {
+	setIsDialogOpen: (value: boolean) => void
+}
+
+export const CreateApplicationForm = ({ setIsDialogOpen }: CreateApplicationFormProps) => {
+	const router = useRouter()
+	const createApplicationMutation = api.application.create.useMutation({
+		onSuccess: (data, variables, context) => {
+			setIsDialogOpen(false)
+			router.refresh()
+			toast.success(`Application for ${variables.companyName} created successfully.`)
+		},
+	})
+
+	const form = useForm<z.infer<typeof insertApplicationSchema>>({
+		resolver: zodResolver(insertApplicationSchema),
+		defaultValues: {
+			companyName: "",
+			status: "pending",
+		},
+	})
+
+	function onSubmit(values: z.infer<typeof insertApplicationSchema>) {
+		createApplicationMutation.mutate(values)
+	}
+
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)}>
+				<FormField
+					control={form.control}
+					name="companyName"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Company Name</FormLabel>
+							<FormControl>
+								<Input placeholder="Company Name" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<FormField
+					control={form.control}
+					name="status"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Status</FormLabel>
+							<FormControl>
+								<Select {...field}>
+									<SelectTrigger className="w-[180px]">
+										<SelectValue placeholder="Select the application status" />
+									</SelectTrigger>
+									<SelectContent>
+										{statusValues.map((value) => (
+											<SelectItem key={value} value={value}>
+												{value}
+											</SelectItem>
+										))}
+									</SelectContent>
+								</Select>
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type="submit" disabled={!form.formState.isValid}>
+					Add
+				</Button>
+			</form>
+		</Form>
+	)
+}
